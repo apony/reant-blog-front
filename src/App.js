a882cd3bc62f267a163dd6f2a5a672acd3c89ed1
Jack <13172211447@163.com>
2019-10-18 00:40:49 +0800

    设置主题颜色

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import MainLayout from './pages/mainLayout'
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.less';
 
 function App () {
   return (
--- src/pages/mainLayout.js
+++ src/pages/mainLayout.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 // import { Route, Redirect }    from 'react-router-dom'
 import './main.css'
 import { getBlogList } from '@/api/blog'
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
 
 class MainLayout extends Component {
@@ -68,11 +68,14 @@ class MainLayout extends Component {
             theme="light"
             mode="horizontal"
             defaultSelectedKeys={['1']}
-            style={{ lineHeight: '64px' }}
+            style={{ lineHeight: '62px' }}
           >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
+            <Menu.Item key="1">
+              <Icon type="home" style={{ fontSize: '16px' }} />
+              首页
+            </Menu.Item>
+            <Menu.Item key="2">nav2</Menu.Item>
+            <Menu.Item key="3">nav3</Menu.Item>
           </Menu>
         </Header>
         <Content style={{ padding: '0 100px' }}>
